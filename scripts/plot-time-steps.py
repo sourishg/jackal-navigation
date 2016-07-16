23c2d61ed584b9e5104fc5e155b72fdebfbcb416
Sourish Ghosh <ragnarok0211@gmail.com>
2016-07-16 17:36:08 -0400

    Update plotting script

--- scripts/plot-time-steps.py
+++ scripts/plot-time-steps.py
@@ -1,14 +1,32 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# create 3 data sets with 1,000 samples
-mu, sigma = 0, 0.04
+elas = []
+pcl = []
+scan = []
 
-x1 = mu + sigma*np.random.randn(1000,1)
-x2 = mu + sigma*np.random.randn(1000,1)
-x3 = mu + sigma*np.random.randn(1000,1)
+data = open('/home/cobot-m/catkin_ws/src/jackal_nav/data/dmap_time.txt', 'r')
+
+for line in data:
+  elas.append(float(line.strip()))
+
+data.close()
+
+data = open('/home/cobot-m/catkin_ws/src/jackal_nav/data/point_cloud_time.txt', 'r')
+
+for line in data:
+  pcl.append(float(line.strip()))
+
+data.close()
+
+data = open('/home/cobot-m/catkin_ws/src/jackal_nav/data/obstacle_scan_time.txt', 'r')
+
+for line in data:
+  scan.append(float(line.strip()))
+
+data.close()
 
 #Stack the data
 plt.figure()
-plt.hist([x1,x2,x3], stacked=True)
+plt.hist([elas,pcl,scan], stacked=True)
 plt.show()
