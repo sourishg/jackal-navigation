801ab964b5775cb14e7dec3a70266a4077a04921
Sourish Ghosh <ragnarok0211@gmail.com>
2016-07-17 18:01:34 -0400

    Plot time for each step in the pipeline

--- scripts/plot-fps.py
+++ scripts/plot-fps.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import math
 
-data = open('/home/cobot-m/catkin_ws/src/jackal_nav/data/dmap_time.txt', 'r')
+data = open('/home/cobot-m/catkin_ws/src/jackal_nav/data/point_cloud_time.txt', 'r')
 
 fps = []
 
 for line in data:
-  fps.append(int(1.0 / float(line.strip())))
+  fps.append(float(line.strip()))
 
 data.close()
 
-plt.hist([f for f in fps], bins = range(200))
+plt.hist([f for f in fps])
 plt.show()
 
 print "AVG: " + str(sum(fps) / len(fps))
