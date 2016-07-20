42220b7305bb8264e3102edd7be824cbe7d75b51
Sourish Ghosh <ragnarok0211@gmail.com>
2016-07-20 18:14:30 -0400

    Clean up and refactor code

--- src/obstacle_avoidance/point_cloud.cpp
+++ src/obstacle_avoidance/point_cloud.cpp
@@ -22,7 +22,7 @@
 using namespace std;
 using namespace cv;
 
-Mat img1, img2, leftim, rightim, leftim_res, rightim_res, show;
+Mat img1, img2, leftim, rightim, leftim_res, rightim_res;
 Mat valid_disp;
 Mat R1, R2, P1, P2, Q;
 Mat K1, K2, R;
@@ -30,7 +30,7 @@ Vec3d T;
 Mat D1, D2;
 Mat XR, XT, V, pos;
 Mat lmapx, lmapy, rmapx, rmapy;
-Mat leftdisp, rightdisp;
+
 image_transport::Publisher disp_pub;
 ros::Publisher pcl_publisher;
 ros::Publisher obstacle_scan_publisher;
@@ -45,7 +45,7 @@ int im_height = 180;
 int crop_offset_x = 0;
 int crop_offset_y = 0;
 int crop_im_width = 270;
-int crop_im_height = 180;
+int crop_im_height = 90;
 const int INF = 1e9;
 uint32_t seq = 0;
 string working_dir;
@@ -60,7 +60,7 @@ const double ROBOT_HEIGHT = 0.34;
 
 bool inImg(int x, int y) {
   // check if pixel lies inside image
-  if (x >= 0 && x < show.cols && y >= 0 && y < show.rows)
+  if (x >= 0 && x < leftim_res.cols && y >= 0 && y < leftim_res.rows)
     return true;
 }
 
@@ -103,39 +103,6 @@ void cacheDisparityValues() {
     }
   }
   cout << "Values cached!" << endl;
-}
-
-void visualizeCriticalRegion() {
-  // visualize region in front of the robot
-  visualization_msgs::Marker line_strip;
-  line_strip.header.frame_id = "jackal";
-  line_strip.header.stamp = ros::Time::now();
-  line_strip.ns = "jackal_obstacle_avoidance";
-  line_strip.action = visualization_msgs::Marker::ADD;
-  line_strip.pose.orientation.w = 1.0;
-  line_strip.id = 0;
-  line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-  line_strip.scale.x = 0.1;
-  line_strip.color.b = 1.0;
-  line_strip.color.a = 1.0;
-  geometry_msgs::Point p;
-  p.x = 0.23;
-  p.y = 0.2;
-  p.z = 0;
-  line_strip.points.push_back(p);
-  p.x = 1.23;
-  p.y = 0.2;
-  p.z = 0;
-  line_strip.points.push_back(p);
-  p.x = 1.23;
-  p.y = -0.2;
-  p.z = 0;
-  line_strip.points.push_back(p);
-  p.x = 0.23;
-  p.y = -0.2;
-  p.z = 0;
-  line_strip.points.push_back(p);
-  marker_pub.publish(line_strip);
 }
 
 void publishObstacleScan(vector< Point3d > points, uint32_t seq) {
@@ -217,8 +184,8 @@ void publishObstacleScan(Mat& show, uint32_t seq) {
   for (int i = 0; i < bin_size; i++) {
     scan[i] = INF;
   }
-  for (int i = 0; i < show.cols; i++) {
-    for (int j = 0; j < show.rows; j++) {
+  for (int i = 0; i < leftim_res.cols; i++) {
+    for (int j = 0; j < leftim_res.rows; j++) {
       int d = show.at<uchar>(j,i);
       if (d < valid_disp.at<Vec2b>(j,i)[0] || d > valid_disp.at<Vec2b>(j,i)[1])
         continue;
@@ -295,8 +262,8 @@ void publishPointCloud(Mat& show, uint32_t seq) {
   pc.header.seq = seq;
   pc.header.frame_id = "jackal";
   pc.header.stamp = ros::Time::now();
-  for (int i = 0; i < show.cols; i++) {
-    for (int j = 0; j < show.rows; j++) {
+  for (int i = 0; i < leftim_res.cols; i++) {
+    for (int j = 0; j < leftim_res.rows; j++) {
       int d = show.at<uchar>(j,i);
       if (d < 2) {
         continue;
@@ -373,69 +340,60 @@ void publishPointCloud(Mat& show, uint32_t seq) {
   publishObstacleScan(points, seq);
 }
 
+Mat generateDisparityMap(Mat& left, Mat& right) {
+  Mat lb, rb;
+  if (left.empty() || right.empty()) 
+    return left;
+  cvtColor(left, lb, CV_BGR2GRAY);
+  cvtColor(right, rb, CV_BGR2GRAY);
+
+  const Size imsize = lb.size();
+  const int32_t dims[3] = {imsize.width,imsize.height,imsize.width}; // bytes per line = width
+
+  Mat leftdpf = Mat::zeros(imsize, CV_32F);
+  Mat rightdpf = Mat::zeros(imsize, CV_32F);
+
+  Elas::parameters param;
+  param.postprocess_only_left = true;
+  Elas elas(param);
+  elas.process(lb.data,rb.data,leftdpf.ptr<float>(0),rightdpf.ptr<float>(0),dims);
+
+  int max_disp = -1;
+  for (int i = 0; i < imsize.width; i++) {
+    for (int j = 0; j < imsize.height; j++) {
+      if (leftdpf.at<uchar>(j,i) > max_disp) max_disp = leftdpf.at<uchar>(j,i);
+    }
+  }
+  for (int i = 0; i < imsize.width; i++) {
+    for (int j = 0; j < imsize.height; j++) {
+      leftdpf.at<uchar>(j,i) = (int)max(255.0*(float)leftdpf.at<uchar>(j,i)/max_disp,0.0);
+    }
+  }
+  Mat show = Mat(crop_im_height, crop_im_width, CV_8UC1, Scalar(0));
+  leftdpf.convertTo(show, CV_8U, 1.);
+  if (!show.empty()) {
+    sensor_msgs::ImagePtr disp_msg;
+    disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", show).toImageMsg();
+    disp_pub.publish(disp_msg);
+  }
+  return show;
+}
+
 void imageCallbackLeft(const sensor_msgs::CompressedImageConstPtr& msg)
 {
   try
   {
-    clock_t begin = clock();
     //Mat tmp = cv_bridge::toCvShare(msg, "mono8")->image;
     Mat tmp = cv::imdecode(cv::Mat(msg->data), CV_LOAD_IMAGE_UNCHANGED);
     if (tmp.empty()) return;
     resize(tmp, img1, rawimsize);
     cv::remap(img1, leftim, lmapx, lmapy, cv::INTER_LINEAR);
     leftim_res = leftim(Rect(crop_offset_x, crop_offset_y, crop_im_width, crop_im_height));
-    //resize(leftim, leftim_res, cropped_imsize);
-    //cv::imshow("view_left", leftim_res);
-    Mat lb,rb;
-    //lb = Mat(cropped_imsize, CV_8UC1, Scalar(0));
-    //rb = Mat(cropped_imsize, CV_8UC1, Scalar(0));
-    if (leftim_res.empty() || rightim_res.empty()) return;
-    cvtColor(leftim_res,lb,CV_BGR2GRAY);
-    cvtColor(rightim_res,rb,CV_BGR2GRAY);
-    int bd = 0;
-
-    const cv::Size imsize = lb.size();
-    const int32_t dims[3] = {imsize.width,imsize.height,imsize.width}; // bytes per line = width
-
-    cv::Mat leftdpf = cv::Mat::zeros(imsize,CV_32F);
-    cv::Mat rightdpf = cv::Mat::zeros(imsize,CV_32F);
-
-    Elas::parameters param;
-    param.postprocess_only_left = false;
-    Elas elas(param);
-
-    elas.process(lb.data,rb.data,leftdpf.ptr<float>(0),rightdpf.ptr<float>(0),dims);
-    //Mat disp;
-    //Mat(leftdpf(cv::Rect(bd,0,leftim.cols,leftim.rows))).copyTo(disp);
-    //leftdpf.copyTo(leftdisp);
-    //disp.convertTo(leftdisp,CV_16S,1);
-    //Mat(rightdpf(cv::Rect(bd,0,leftim.cols,leftim.rows))).copyTo(disp);
-    //rightdpf.copyTo(disp);
-    //disp.convertTo(rightdisp,CV_16S,16);
     
-    int max_disp = -1;
-    for (int i = 0; i < imsize.width; i++) {
-      for (int j = 0; j < imsize.height; j++) {
-        if (leftdpf.at<uchar>(j,i) > max_disp) max_disp = leftdpf.at<uchar>(j,i);
-      }
-    }
-    for (int i = 0; i < imsize.width; i++) {
-      for (int j = 0; j < imsize.height; j++) {
-        leftdpf.at<uchar>(j,i) = (int)max(255.0*(float)leftdpf.at<uchar>(j,i)/max_disp,0.0);
-      }
-    }
-    
-    show = Mat(crop_im_height, crop_im_width, CV_8UC1, Scalar(0));
-    leftdpf.convertTo(show, CV_8U, 1.);
-    /*
-    if (dmaps.size() < 2) {
-      dmaps.push_back(show);
-    } else {
-      dmaps.pop_front();
-      dmaps.push_back(show);
-      filterDisparityMap();
-    }
-    */
+    clock_t begin = clock();
+
+    Mat dmap = generateDisparityMap(leftim_res, rightim_res);
+
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     if (logging) {
@@ -452,35 +410,9 @@ void imageCallbackLeft(const sensor_msgs::CompressedImageConstPtr& msg)
 
       time_log.dmap_time = elapsed_secs;
     }
-    if (!show.empty()) {
-      //filterDisparityMap(show);
-      sensor_msgs::ImagePtr disp_msg;
-      //if (dmaps.size() < 2)
-      disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", show).toImageMsg();
-      //else
-      //disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", dmaps.back()).toImageMsg();
-      disp_pub.publish(disp_msg);
-      //if (dmaps.size() < 2)
 
-      publishPointCloud(show, seq);
-      
-      //else
-      //publishPointCloud(dmaps.back());
-      //imshow("view_disp", show);
-    }
-    //visualizeCriticalRegion();
-    /*
-    if (cv::waitKey(30) > 0) {
-      printf("Saving scene!\n");
-      char leftfile[200], rightfile[200], dispfile[200];
-      sprintf(leftfile, "left%d.png", save_count);
-      sprintf(rightfile, "right%d.png", save_count);
-      sprintf(dispfile, "disp%d.png", save_count);
-      cv::imwrite(leftfile, leftim_res);
-      cv::imwrite(rightfile, rightim_res);
-      cv::imwrite(dispfile, show);
-      save_count++;
-    }*/
+    if (!dmap.empty())
+      publishPointCloud(dmap, seq);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -498,20 +430,6 @@ void imageCallbackRight(const sensor_msgs::CompressedImageConstPtr& msg)
     resize(tmp, img2, rawimsize);
     cv::remap(img2, rightim, rmapx, rmapy, cv::INTER_LINEAR);
     rightim_res = rightim(Rect(crop_offset_x, crop_offset_y, crop_im_width, crop_im_height));
-    //resize(rightim, rightim_res, Size(270, 180));
-    /*
-    cv::imshow("view_right", rightim_res);
-    if (cv::waitKey(30) > 0) {
-      printf("Saving scene!\n");
-      char leftfile[200], rightfile[200], dispfile[200];
-      sprintf(leftfile, "left%d.png", save_count);
-      sprintf(rightfile, "right%d.png", save_count);
-      sprintf(dispfile, "disp%d.png", save_count);
-      cv::imwrite(leftfile, leftim_res);
-      cv::imwrite(rightfile, rightim_res);
-      cv::imwrite(dispfile, show);
-      save_count++;
-    }*/
   }
   catch (cv_bridge::Exception& e)
   {
@@ -530,9 +448,7 @@ int main(int argc, char **argv)
   if (logging) {
     time_log_publisher = nh.advertise<jackal_nav::JackalTimeLog>("/Jackal/TimeLog", 1);
   }
-  //cv::namedWindow("view_left");
-  //cv::namedWindow("view_right");
-  //cv::namedWindow("view_disp");
+  
   working_dir = argv[1];
   string calib_file = working_dir;
   calib_file.append("src/calibration/stereo_calib.yml");
@@ -564,12 +480,9 @@ int main(int argc, char **argv)
   cv::initUndistortRectifyMap(K2, D2, R2, P2, img2.size(), CV_32F, rmapx, rmapy);
 
   cacheDisparityValues();
-  //cv::startWindowThread();
+
   ros::Subscriber subl = nh.subscribe("/webcam/left/image_raw/compressed", 1, imageCallbackLeft);
   ros::Subscriber subr = nh.subscribe("/webcam/right/image_raw/compressed", 1, imageCallbackRight);
 
   ros::spin();
-  //cv::destroyWindow("view_left");
-  //cv::destroyWindow("view_right");
-  //cv::destroyWindow("view_disp");
 }
